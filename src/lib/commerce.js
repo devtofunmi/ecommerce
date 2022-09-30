e1142f38f4d35a87d5dfff000dd25c0ef58259c8
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-09-30 21:12:21 +0100

    import the api from env

---
+++ src/lib/commerce.js
@@ -0,0 +1,19 @@
+import Commerce from '@chec/commerce.js';
+
+const checAPIKey = import.meta.env.VITE_APP_CHEC_PUBLIC_KEY;
+
+const commerceConfig = {
+  axiosConfig: {
+    headers: {
+      'X-Chec-Agent': 'commerce.js/v2',
+      'Chec-Version': '2021-09-29',
+    },
+  },
+};
+
+
+
+export default new Commerce(
+  checAPIKey,
+  commerceConfig,
+);
