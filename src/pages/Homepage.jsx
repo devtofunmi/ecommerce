77c91a6e4ff5989556ff7cb5def1f66c060a0893
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-01 03:35:37 +0100

    fetch from commercejs,add truncate function and import product card

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
+import { Box, Flex, Input } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import commerce from "../lib/commerce";
-// import ProductsList from "./components/ProductsList";
+import ProductsCard from "./ProductsCard";
 
 const Homepage = () => {
   const [products, setProducts] = useState([]);
@@ -20,7 +21,30 @@ const Homepage = () => {
   useEffect(() => {
     fetchProducts();
   }, []);
-  return <div>Homepage</div>;
+
+  function truncateString(str) {
+    if (str.length <= 15) {
+      return str;
+    } else return str.slice(0, 15) + "...";
+  }
+  return (
+    <>
+      <Flex w="30%" mt={10} justifyContent="center" alignItems="center">
+        <Input type="text" placeholder="search" />
+      </Flex>
+      <Box>
+        {products.map((product) => (
+          <ProductsCard
+            key={product.id}
+            image={product.image.url}
+            name={truncateString(product.name)}
+            price={product.price.formatted_with_symbol}
+            description={product.description}
+          />
+        ))}
+      </Box>
+    </>
+  );
 };
 
 export default Homepage;
