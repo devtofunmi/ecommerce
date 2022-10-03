c8da57507db5def809eb17c85782f5afe84d9b06
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-03 08:49:10 +0100

    passing the product to the localstorage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from "react";
 import commerce from "../lib/commerce";
 import ProductsCard from "./ProductsCard";
 import { BsFillCartCheckFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   const [products, setProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [cart, setCart] = useState([]);
 
   const fetchProducts = () => {
     commerce.products
@@ -19,6 +21,7 @@ const Homepage = () => {
         console.log("There was an error fetching the products", error);
       });
   };
+
   const filteredData = () => {
     if (!searchTerm) {
       return products;
@@ -31,8 +34,21 @@ const Homepage = () => {
 
   useEffect(() => {
     fetchProducts();
+    if (!localStorage.getItem("cart")) {
+      localStorage.setItem("cart", JSON.stringify([]));
+    }
   }, []);
 
+  const addToCart = (name, image, price, id) => {
+    const product = { name, image, price, id };
+    const cart = JSON.parse(localStorage.getItem("cart"));
+
+    if (cart.includes(product)) {
+      alert("Already added to cart");
+    } else {
+      localStorage.setItem("cart", JSON.stringify([...cart, product]));
+    }
+  };
   return (
     <>
       <Center>
@@ -46,7 +62,9 @@ const Homepage = () => {
           }}
         />
         <Box mt={"15px"} ml="15px">
-          <BsFillCartCheckFill size={"40px"} />{" "}
+          <Link to={"/cart"}>
+            <BsFillCartCheckFill size={"40px"} />
+          </Link>
         </Box>
       </Center>
 
@@ -59,6 +77,7 @@ const Homepage = () => {
             price={product.price.formatted_with_symbol}
             description={product.description}
             id={product.id}
+            addToCart={addToCart}
           />
         ))}
       </Flex>
