858d98e740ad65915ef2df97abda71ac7d4a329f
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-03 08:52:46 +0100

    import addtocart as props and the link to the box

--- src/pages/ProductsCard.jsx
+++ src/pages/ProductsCard.jsx
@@ -1,39 +1,60 @@
 import { Box, Button, Flex, Heading, Img, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const ProductsCard = ({ name, image, price, id }) => {
+const ProductsCard = ({ name, image, price, id, addToCart }) => {
   function truncateString(str) {
     if (str.length <= 15) {
       return str;
     } else return str.slice(0, 15) + "...";
   }
-  return (
-    <Link to={`/${id}`}>
-      <Flex
-        w="300px"
-        h="350px"
-        m="30px"
-        boxShadow={"base"}
-        flexDirection={"column"}
-        alignItems={"center"}
-        borderRadius={"md"}
-        bg={"blackAlpha.500"}
-        justifyContent={"space-between"}
-      >
-        <Box w={"100%"} h={"200px"}>
-          <Img src={image} objectFit={"cover"} width={"100%"} height={"100%"} />
-        </Box>
-        <Heading fontSize={"2xl"}>{truncateString(name)}</Heading>
-        <Text fontWeight={"bold"} fontSize={"xl"}>
-          {price}
-        </Text>
 
-        <Button mt="30px" w={"100%"} bg={"blue.300"}>
-          Add to Cart
-        </Button>
-      </Flex>
-    </Link>
+  return (
+    <Flex
+      w="300px"
+      h="350px"
+      m="30px"
+      boxShadow={"base"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      borderRadius={"md"}
+      bg={"blackAlpha.500"}
+      justifyContent={"space-between"}
+    >
+      <Link to={`/${id}`}>
+        <Flex
+          w={"100%"}
+          h={"100%"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          borderRadius={"md"}
+        >
+          <Box w={"100%"} h={"200px"}>
+            <Img
+              src={image}
+              objectFit={"cover"}
+              width={"100%"}
+              height={"100%"}
+            />
+          </Box>
+          <Heading fontSize={"2xl"}>{truncateString(name)}</Heading>
+          <Text fontWeight={"bold"} fontSize={"xl"}>
+            {price}
+          </Text>
+        </Flex>
+      </Link>
+
+      <Button
+        mt="30px"
+        w={"100%"}
+        bg={"blue.300"}
+        onClick={(e) => {
+          addToCart(name, image, price, id);
+        }}
+      >
+        Add to Cart
+      </Button>
+    </Flex>
   );
 };
 
